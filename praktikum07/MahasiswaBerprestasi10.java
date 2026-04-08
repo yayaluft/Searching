7616ea82ac599a0bb427d4e1227c83ca4aeaebf0
yayaluft <syanuha66@gmail.com>
2026-04-08 09:38:53 +0700

    Modifikasi Binary search DESC Pertanyaan No.5

--- praktikum07/MahasiswaBerprestasi10.java
+++ praktikum07/MahasiswaBerprestasi10.java
@@ -94,7 +94,7 @@ public class MahasiswaBerprestasi10 {
             mid = (left + right) / 2;
             if (cari == listMhs[mid].ipk) {
                 return (mid);
-            } else if (listMhs[mid].ipk > cari) {
+            } else if (listMhs[mid].ipk < cari) {
                 return findBinarySearch(cari, left, mid - 1);
             } else {
                 return findBinarySearch(cari, mid + 1, right);
